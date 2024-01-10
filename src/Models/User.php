7dd3d04c470221c184bf800766004da9dfa76ca2
Alexander Petrov <petrov.ale@promail366.com>
2024-01-10 15:46:26 +0200

    add tasks 5

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Collection;
 use Uru\BitrixModels\Models\UserModel;
 
 class User extends UserModel
@@ -16,5 +17,17 @@ class User extends UserModel
     public function getName(): string
     {
         return $this['NAME'];
+    }
+
+    // перезаписанный метод возвращающий пользователей из БД
+    public static function GetList(): Collection
+    {
+        return new Collection(
+            [
+                new User(1, ['NAME' => 'John Doe']),
+                new User(2, ['NAME' => 'Alan M']),
+                new User(2, ['NAME' => 'Mike M']),
+            ]
+        );
     }
 }
